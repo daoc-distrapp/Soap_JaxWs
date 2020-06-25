28c9393ddadee1f0fc54a459dd06fe7a4a6188a5
Diego Ordóñez <diego.ordonez@gmail.com>
2020-06-25 11:54:06 -0500

    Update

--- Soap_JaxWs/src/main/java/daoc/soapjaxws/EmpleadoService.java
+++ Soap_JaxWs/src/main/java/daoc/soapjaxws/EmpleadoService.java
@@ -10,24 +10,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@WebService(endpointInterface = "daoc.soapjaxws.I_EmpleadoService")
-public class EmpleadoService implements I_EmpleadoService {
+@WebService(serviceName = "EmpService",
+			name = "Emp",
+			portName = "EmpPort",
+			targetNamespace = "http://soapjaxws.daoc/")
+public class EmpleadoService {
 
 	@Autowired
 	public EmpleadoRepository repository;	
 	
-	@Override
 	public Empleado getEmpleado(int id) {
 		Optional<Empleado> opt = repository.findById(id);
 		return opt.orElse(null);
 	}
-	@Override
 	public List<Empleado> getAllEmpleados() {
 	    List<Empleado> result = new ArrayList<>();
 	    repository.findAll().forEach(result::add);
 		return result;
 	}
-	@Override
 	public void createEmpleado(String nombre, String cargo) {
 		System.out.println(String.format("Empleado [nombre: %s, cargo: %s]", nombre, cargo));
 		repository.save(new Empleado(nombre, cargo));
